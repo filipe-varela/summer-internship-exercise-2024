4959b253f7d5425d4f1e30932558203acefed06a
Filipe Varela <43725698+filipe-varela@users.noreply.github.com>
2024-05-16 15:28:09 +0100

    Implements getTeknonymy search
    Implements logic behind the getTeknonymy search, where it searches in
    each recent generation the oldest children. This logic is based on Breadth-First Search
    algorithm, without the verification part since a family member isn't visited more
    than once (otherwise the it would be a very unique family tree).
    
    Adds additional documentation for the getRelativeTeknonymy.

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -1,10 +1,34 @@
 package com.premiumminds.internship.teknonymy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.premiumminds.internship.teknonymy.Person;
 
 class TeknonymyService implements ITeknonymyService {
 
-  String getRelativeTeknonymy(Character sex, int level) {
+  /**
+   * <p>
+   * Get corresponding relative teknonymy given the sex and level in the family
+   * tree. That is, if:
+   * </p>
+   * <p>
+   * - level == 1: Parent
+   * </p>
+   * <p>
+   * - level == 2: Grandparent
+   * </p>
+   * <p>
+   * - level >= 3: ((level-2) * Great-)Grandparent
+   * </p>
+   * 
+   * @param sex   either 'M' for male or 'F' for female
+   * @param level integer value for the current family tree level
+   * @return String with the corresponding relative teknonymy
+   */
+  String getRelativeTeknonymy(final Character sex, final int level) {
+    if (level < 1)
+      throw new IllegalArgumentException("The argument level = " + level + " must be greater than zero.");
     String teknonymy = "";
     if (level > 1) {
       teknonymy = "grand";
@@ -14,6 +38,52 @@ class TeknonymyService implements ITeknonymyService {
     return (sex == 'M') ? teknonymy + "father" : teknonymy + "mother";
   }
 
+  private boolean doesNotHaveChildren(Person person) {
+    final Person[] children = person.children();
+    return children == null || children.length == 0;
+  }
+
+  Person getRecentOldestChildren(final Person root) {
+    final List<Person> personQueue = new ArrayList<Person>();
+    personQueue.add(root.withLevel(0));
+
+    personQueue.get(0).incrementChildrenLevel();
+
+    Person recentOldestPerson = new Person(personQueue.get(0));
+    Person currentPerson = new Person(personQueue.get(0));
+
+    while (!personQueue.isEmpty()) {
+      currentPerson = personQueue.remove(0);
+
+      // validate oldest and recent children
+      if (!currentPerson.equals(recentOldestPerson)) {
+        if (currentPerson.isMoreRecentThan(recentOldestPerson)) {
+          recentOldestPerson = new Person(currentPerson);
+        } else if (currentPerson.sameGenerationAs(recentOldestPerson)) {
+          if (currentPerson.isOlderThan(recentOldestPerson)) {
+            recentOldestPerson = new Person(currentPerson);
+          }
+        }
+      }
+
+      if (doesNotHaveChildren(currentPerson))
+        continue;
+
+      for (Person person : currentPerson.children()) {
+        if (!doesNotHaveChildren(person)) {
+          // Update children level, if any
+          person.incrementChildrenLevel();
+        }
+
+        // Add to queue since it has children
+        personQueue.add(0, new Person(person));
+      }
+    }
+
+    return recentOldestPerson;
+
+  }
+
   /**
    * Method to get a Person Teknonymy Name
    * 
@@ -21,15 +91,15 @@ class TeknonymyService implements ITeknonymyService {
    * @return String which is the Teknonymy Name
    */
   public String getTeknonymy(Person person) {
-    Person[] children = person.children();
-
     // In case the person doesn't havee children
-    if (children == null || person.children().length == 0)
+    if (doesNotHaveChildren(person))
       return "";
 
     Character sex = person.sex();
-    String name = "";
+    Person finalChildren = getRecentOldestChildren(person);
+    String name = finalChildren.name();
+    int level = finalChildren.level();
 
-    return getRelativeTeknonymy(sex, 0) + " of " + name;
+    return getRelativeTeknonymy(sex, level) + " of " + name;
   };
 }
