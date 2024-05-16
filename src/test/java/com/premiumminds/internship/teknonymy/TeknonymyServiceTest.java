172b804f72fa12f7f36fd34ef6a4d673fcd0f1c6
Filipe Varela <43725698+filipe-varela@users.noreply.github.com>
2024-05-16 12:45:25 +0100

    Adds test to getRelativeTeknonymy
    Adds test for the getTeknonymy method, evaluating from parent to
    great-great-grandparent. Following great-grandparents would be
    unnecessary due to repetition.

--- src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
+++ src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
@@ -54,4 +54,61 @@ public class TeknonymyServiceTest {
     String expected = "father of Holy";
     assertEquals(result, expected);
   }
+
+  @Test
+  public void FatherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('M', 1);
+    String expected = "father";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void MotherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('F', 1);
+    String expected = "mother";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void GrandFatherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('M', 2);
+    String expected = "grandfather";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void GrandMotherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('F', 2);
+    String expected = "grandmother";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void GreatGrandFatherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('M', 3);
+    String expected = "great-grandfather";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void GreatGrandMotherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('F', 3);
+    String expected = "great-grandmother";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void GreatGreatGrandFatherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('M', 4);
+    String expected = "great-great-grandfather";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void GreatGreatGrandMotherTest() {
+    String result = new TeknonymyService().getRelativeTeknonymy('F', 4);
+    String expected = "great-great-grandmother";
+    assertEquals(result, expected);
+  }
+
 }
