d995871efd85e6eb6891fa75e71610a276359ffb
Filipe Varela <43725698+filipe-varela@users.noreply.github.com>
2024-05-16 15:23:19 +0100

    Adds level to the Person record
    Adds an integer level field to the Person record, defaulted to zero when not
    provided. This field specifies the family tree's level the Person object is situated in.
    
    Adds an additional copy method, withLevel, to create copy variants of Person with
    different level value. Also, adds an incrementChildrenLevel method to
    propagate an increment version of the parent level to its children.
    
    Besides that, it adds specific comparison methods to ease the search process.

--- src/main/java/com/premiumminds/internship/teknonymy/Person.java
+++ src/main/java/com/premiumminds/internship/teknonymy/Person.java
@@ -2,5 +2,49 @@ package com.premiumminds.internship.teknonymy;
 
 import java.time.LocalDateTime;
 
-public record Person(String name,Character sex, Person[] children, LocalDateTime dateOfBirth) {
+public record Person(
+        String name,
+        Character sex,
+        Person[] children,
+        LocalDateTime dateOfBirth,
+        int level) {
+    public Person(String name, Character sex, Person[] children, LocalDateTime dateOfBirth) {
+        this(name, sex, children, dateOfBirth, 0);
+    }
+
+    public Person(Person newPerson) {
+        this(
+                newPerson.name(),
+                newPerson.sex(),
+                newPerson.children(),
+                newPerson.dateOfBirth(),
+                newPerson.level());
+    }
+
+    public Person withLevel(int newLevel) {
+        return new Person(
+                this.name,
+                this.sex,
+                this.children,
+                this.dateOfBirth,
+                newLevel);
+    }
+
+    public boolean isMoreRecentThan(Person test) {
+        return test.level() < this.level;
+    }
+
+    public boolean isOlderThan(Person test) {
+        return this.dateOfBirth.isBefore(test.dateOfBirth());
+    }
+
+    public boolean sameGenerationAs(Person test) {
+        return test.level() == this.level;
+    }
+
+    public void incrementChildrenLevel() {
+        for (int i = 0; i < children.length; i++) {
+            children[i] = children[i].withLevel(level + 1);
+        }
+    }
 }
